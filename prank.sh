4a0796fece209823648a57d87577eb532cbf2ac0
dhruvdhawan97 <36608696+dhruvdhawan97@users.noreply.github.com>
2020-03-08 19:29:54 -0400

    Update prank.sh

--- prank.sh
+++ prank.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 a=1
-b=1000000
+b=1000000 #change the number to desired value (make # smaller for quick results or higher for slower results)
 for a in {1..1000000}
  do
-	if [ $a -eq $b ]
+	if [ $a -eq $b ] #compared the 2 variables (count is to a million so it takes a while to change the background)
 	then
 	   gsettings set org.gnome.desktop.background picture-uri "https://wallpaperplay.com/walls/full/0/8/1/44309.jpg"
+	   # add desired file/link of the jpeg/jpg/png etc. instead of link in quotation marks
 
 	fi
  done
